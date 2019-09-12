adce9a354cec66235c0fd8801a0f5814f7979fd4
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-09-12 10:09:04 -0400

    header component connected to app, displaying correctly

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
+    <View style={styles.screen}>
+      <Header title="Guess a number"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- components/header.js
+++ components/header.js
@@ -13,7 +13,14 @@ const styles = StyleSheet.create({
     header: {
         width: '100%',
         height: 90,
-        paddingTop: 36
+        paddingTop: 36,
+        backgroundColor: '#f7287b',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerText: {
+        color: 'black',
+        fontSize: 18
     }
 })
 
