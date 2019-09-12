34976c7d16e2f4562cb21bfbd49219415948b752
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-09-12 10:54:44 -0400

    styles added to startGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>Start a new game!</Text>
+            <Text style={styles.title}>Start a new game!</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title="reset" onPress={()=>{}}/>
+                    <Button title="confirm" onPress={()=>{}}/>
+                </View>
+            </View>
         </View>
     );
 };
@@ -14,8 +22,31 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center',
-        
-    }
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 5,
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10
+
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
 });
 
 export default StartGameScreen;
